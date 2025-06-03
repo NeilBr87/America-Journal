420249043007ca428ef7c98fac968b17cc199781
Neil Brooks <neil.brooks87@gmail.com>
2025-06-03 20:31:57 +0100

    Alt prop

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,7 +17,7 @@ function App() {
           <h2>Sorry! This project is all about mobile-first development.</h2>
           <h4>Please feel free to review this content on a mobile device or by using browser tools.</h4>
           <h4>Expected ETA for desktop version of this: June 2025.</h4>
-          <img id="desktopImg" src="https://deckerdevs.com/hubfs/mobile-first-mobile-last-meme.jpg"></img>
+          <img id="desktopImg" alt="meme" src="https://deckerdevs.com/hubfs/mobile-first-mobile-last-meme.jpg"></img>
         </div>}
     </div>
   );
